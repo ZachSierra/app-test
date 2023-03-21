931e2db1cf30ebc0fa9b6f4acbb07e530385cf7d
Zach <sierrazja@gmail.com>
2023-03-21 17:05:32 -0500

    Attempt to commit in github

---
+++ app/src/main/java/com/example/app/University.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+import java.util.ArrayList;
+
+public class University {
+    private String name;
+    private String uniEmail;
+    ArrayList<User> U;
+    public University(String name){
+        this.name = name;
+    }
+}
